8048c81d62e5ed5e5f46a67e7a16bd201b1320f2
unknown <phengdavid33@gmail.com>
2021-05-10 22:43:56 +0700

    second try

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use("/api/v1/auth", auth);
 app.use("/api/v1/lists", lists);
 app.use(errorHandler);
 if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
