c0afc3d87183bc68b6efb1a7099b1885e48872be
unknown <phengdavid33@gmail.com>
2021-05-10 22:47:39 +0700

    third

--- server.js
+++ server.js
@@ -20,9 +20,11 @@ app.use("/api/v1/auth", auth);
 app.use("/api/v1/lists", lists);
 app.use(errorHandler);
 if (process.env.NODE_ENV === "production") {
+  // Serve any static files
   app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 // start server
